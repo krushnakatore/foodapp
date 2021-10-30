64ab565702c637254b06f945e00eaee4c3ad3c9f
krushnakatore <91548932+krushnakatore@users.noreply.github.com>
2021-10-30 13:43:11 +0530

    Add files via upload

---
+++ show.js
@@ -0,0 +1,39 @@
+async function getData(url){
+
+    let res = await fetch(url)
+
+    let data = await res.json()
+
+    return data;
+
+
+
+}
+
+
+function append(data,container){
+
+   data.forEach(({strMeal,strMealThumb})=>{
+
+        let div = document.createElement("div")
+
+        let p = document.createElement('p')
+
+        p.innerText = strMeal;
+
+        let img = document.createElement("img");
+
+         img.src = strMealThumb;
+
+        div.append(img,p);
+
+
+        container.append(div);
+
+
+    })
+
+
+}
+
+export {getData,append}
