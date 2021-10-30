4663b835bed7ca6c0bd7329750aaa4f1ff1becec
krushnakatore <91548932+krushnakatore@users.noreply.github.com>
2021-10-30 13:44:13 +0530

    Add files via upload

---
+++ navbaroffoodapp.js
@@ -0,0 +1,19 @@
+function navbar1(){
+    return `<h3> <a href ="latest.html">latest Meal</a></h3>
+
+    <div id = "options">
+
+        
+       
+        
+        <h3> <a href ="dealsofday.html">Meals of the Day</a></h3>
+        <h3> <a href ="searchmeal.html">SearchMeal</a></h3>
+        <h3> <a href ="">Sign In</a></h3>
+        <h3> <a href ="">Sign Up</a></h3>
+
+    
+
+</div>`
+}
+
+export default navbar1;
